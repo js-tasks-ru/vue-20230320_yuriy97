84a0d375d10aeae6e034cc88ad420b590ad6cf3c
Юрий Андреев <y_andreev@mail.ru>
2023-04-30 17:56:43 +0300

    10.2

--- 10-slots/02-PromiseWrapper/components/PromiseWrapper.vue
+++ 10-slots/02-PromiseWrapper/components/PromiseWrapper.vue
@@ -1,5 +1,5 @@
 <template>
-  <!-- -->
+  <slot :name="status" :error="error" :result="result"  />
 </template>
 
 <script>
@@ -12,5 +12,30 @@ export default {
       required: true,
     },
   },
+
+  data(){
+
+    return{
+      name: null,
+      error:null,
+      result:null,
+    }
+  },
+
+  watch: {
+    promise: {
+      immediate: true,
+      handler(){
+        this.status = 'pending';
+        this.promise.then((response) =>{
+          this.status = 'fulfilled';
+          this.result = response;
+        }).catch((err) =>{
+          this.status = 'rejected'
+          this.error = err
+        }); 
+      },
+    }
+  }
 };
 </script>
